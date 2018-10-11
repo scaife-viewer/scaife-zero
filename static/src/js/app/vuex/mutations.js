9be3fadcedcc10e510cdc8a6eb13b873da1db3ce
Patrick Altman <paltman@gmail.com>
2018-10-10 23:30:32 -0500

    Use single-quotes in javascript

--- static/src/js/app/vuex/mutations.js
+++ static/src/js/app/vuex/mutations.js
@@ -1,4 +1,4 @@
-import { TOGGLE_SIDEBAR_LEFT, TOGGLE_SIDEBAR_RIGHT } from "../constants";
+import { TOGGLE_SIDEBAR_LEFT, TOGGLE_SIDEBAR_RIGHT } from '../constants';
 
 export default {
   [TOGGLE_SIDEBAR_LEFT]: state => state.sidebarLeftOpened = !state.sidebarLeftOpened,
