817f030ffb272ef1a96dc0d0a926dea94e792253
James Tauber <jtauber@jtauber.com>
2018-10-11 01:42:03 -0400

    more consistent indentation

--- static/src/js/app/layout/Skeleton.vue
+++ static/src/js/app/layout/Skeleton.vue
@@ -3,7 +3,7 @@
 
     <div :class="['sidebar', { collapsed: !sidebarLeftOpened, 'both-opened': sidebarLeftOpened && sidebarRightOpened }]" id="left-sidebar">
       <button class="close-left" v-if="sidebarLeftOpened" @click.prevent="toggleLeftSidebar">
-          <icon name="arrow-left"></icon>
+        <icon name="arrow-left"></icon>
       </button>
       <div>
         <slot name="left"></slot>
@@ -11,7 +11,7 @@
     </div>
 
     <button class="open-left" v-if="!sidebarLeftOpened" @click.prevent="toggleLeftSidebar">
-        <icon name="arrow-right"></icon>
+      <icon name="arrow-right"></icon>
     </button>
 
     <section id="content_body">
@@ -19,12 +19,12 @@
     </section>
 
     <button class="open-right" v-if="!sidebarRightOpened" @click.prevent="toggleRightSidebar">
-        <icon name="arrow-left"></icon>
+      <icon name="arrow-left"></icon>
     </button>
 
     <div :class="['sidebar', { collapsed: !sidebarRightOpened, 'both-opened': sidebarLeftOpened && sidebarRightOpened }]" id="right-sidebar">
       <button class="close-right" v-if="sidebarRightOpened" @click.prevent="toggleRightSidebar">
-          <icon name="arrow-right"></icon>
+        <icon name="arrow-right"></icon>
       </button>
       <div>
         <slot name="right"></slot>
@@ -48,10 +48,10 @@ export default {
   },
   methods: {
     toggleLeftSidebar() {
-        this.$store.dispatch(TOGGLE_SIDEBAR_LEFT);
+      this.$store.dispatch(TOGGLE_SIDEBAR_LEFT);
     },
     toggleRightSidebar() {
-        this.$store.dispatch(TOGGLE_SIDEBAR_RIGHT);
+      this.$store.dispatch(TOGGLE_SIDEBAR_RIGHT);
     }
   },
 };
