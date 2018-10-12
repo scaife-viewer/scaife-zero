28e4f7371bd684ebcae4310d49f77d69b9be9edb
Patrick Altman <paltman@gmail.com>
2018-10-11 21:27:10 -0500

    Hook up CI and fixed some python lints

--- zero/context_processors.py
+++ zero/context_processors.py
@@ -1,7 +1,8 @@
 import socket
 
-from django.contrib.sites.models import Site
 from django.conf import settings as django_settings
+
+from django.contrib.sites.models import Site
 
 
 def pinax_apps_filter(app):
