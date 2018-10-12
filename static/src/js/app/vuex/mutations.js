656e9207d58b38fb2ee08562f81ef8bfc62dfe02
Patrick Altman <paltman@gmail.com>
2018-10-11 21:12:16 -0500

    Manual eslint fixes

--- static/src/js/app/vuex/mutations.js
+++ static/src/js/app/vuex/mutations.js
@@ -1,6 +1,10 @@
 import { TOGGLE_SIDEBAR_LEFT, TOGGLE_SIDEBAR_RIGHT } from '../constants';
 
 export default {
-  [TOGGLE_SIDEBAR_LEFT]: state => state.sidebarLeftOpened = !state.sidebarLeftOpened,
-  [TOGGLE_SIDEBAR_RIGHT]: state => state.sidebarRightOpened = !state.sidebarRightOpened,
+  [TOGGLE_SIDEBAR_LEFT]: (state) => {
+    state.sidebarLeftOpened = !state.sidebarLeftOpened;
+  },
+  [TOGGLE_SIDEBAR_RIGHT]: (state) => {
+    state.sidebarRightOpened = !state.sidebarRightOpened;
+  },
 };
